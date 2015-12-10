29577f133c2a6b2ecbf58d0eb8dc4ea9bf957086
dusha <dusha8262@gmail.com>
2015-12-10 15:31:41 +0600

    change function changeHospitalList

--- js/main.js
+++ js/main.js
@@ -470,7 +470,6 @@
     }
 
     function initHospitalList(hospitalNames){
-
         hospitalListContainer.div.selectAll('ul')
             .remove();
 
@@ -509,17 +508,17 @@
             })
         ;
 
-        function addHideItemOrValue(item){
+        function addHideItem(item){
             if (item.key === d){
                 return item
             }
         }
-        function updateHideItemOrValue(item){
+        function updateHideItem(item){
             if (item.key !== d){
                 return item
             }
         }
-        function hideItemOrValue(item){
+        function hideItem(item){
             if(item){
                 if (item.key !== d){
                     return item
@@ -530,47 +529,19 @@
         }
 
         if (addOrHideData){
-            var addItem = hideItems.filter(addHideItemOrValue)[0];
-            hideItems = hideItems.filter(updateHideItemOrValue);
-
-            var addValue = hideValues.filter(addHideItemOrValue)[0];
-            hideValues = hideValues.filter(updateHideItemOrValue);
-
+            var addItem = hideItems.filter(addHideItem)[0];
             selectedData.items.push(addItem);
-            selectedData.values.push(addValue);
+            hideItems = hideItems.filter(updateHideItem);
+
+            selectedData.items = selectedData.values.filter(function(item, i){
+                if (selectedData.items.indexOf(item) !== -1){
+                    return item
+                }
+            });
         }else{
-            selectedData.items = selectedData.items.filter(hideItemOrValue);
-            selectedData.values = selectedData.values.filter(hideItemOrValue);
+            selectedData.items = selectedData.items.filter(hideItem);
         }
 
-        function deromanize (str) {
-            var	str = str.toUpperCase(),
-                validator = /^M*(?:D?C{0,3}|C[MD])(?:L?X{0,3}|X[CL])(?:V?I{0,3}|I[XV])$/,
-                token = /[MDLV]|C[MD]?|X[CL]?|I[XV]?/g,
-                key = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
-                num = 0, m;
-            if (!(str && validator.test(str)))
-                return false;
-            while (m = token.exec(str))
-                num += key[m[0]];
-            return num;
-        }
-
-        selectedData.items.sort(function(itemA, itemB){
-            try {
-                var numberItemA = /(\w+)\..*/g.exec(itemA.key)[1];
-            } catch(err) {
-                return 1
-            }
-            try {
-                var numberItemB = /(\w+)\..*/g.exec(itemB.key)[1];
-            } catch(err) {
-                return -1
-            }
-
-
-            return +deromanize(numberItemA) - (+deromanize(numberItemB));
-        });
         selectedData && makeSurface(selectedData);
         unsetWait();
     }
